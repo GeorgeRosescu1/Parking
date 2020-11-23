554bbbfdcc16f549eb6a21e212016906c7ad493c
George Rosescu <george.rosescu@gmail.com>
2020-11-23 18:06:08 +0200

    Initial Commit

---
+++ Parking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parking
+//
+//  Created by George Rosescu on 23.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
